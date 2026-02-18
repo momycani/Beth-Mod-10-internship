7d995f1f3fe000dd59c54b90ca3ff74ae92d68ab
momycani <momycani>
2026-02-18 14:58:56 -0800

    home page complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Routes, Route, Navigate } from "react-router-dom";
 
 import "./styles/style.css"
@@ -9,11 +9,23 @@ import Player from "./pages/Player";
 import ChoosePlan from "./pages/ChoosePlan";
 import Settings from "./pages/Settings";
 import Library from "./pages/Library"; 
+import LoginModal from "./components/LoginModal.jsx";
+import SignUpModal from "./components/SignUpModal.jsx";
 
 export default function App() {
-  return (    
+  const [loginOpen, setLoginOpen] = useState(false);
+  const [signUpOpen, setSignUpOpen] = useState(false);
+
+  const openLogin = () => { setSignUpOpen(false); setLoginOpen(true); };
+  const openSignUp = () => { setLoginOpen(false); setSignUpOpen(true); };
+
+  const closeLogin = () => setLoginOpen(false);
+  const closeSignUp = () => setSignUpOpen(false);
+
+  return ( 
+    <>   
     <Routes>
-      <Route path="/" element={<Home />} />       
+      <Route path="/" element={<Home onLoginClick={openLogin} />} />      
       <Route path="/for-you" element={<ForYou />} />
       <Route path="/book/:id" element={<Book />} />
       <Route path="/player/:id" element={<Player />} />
@@ -21,7 +33,11 @@ export default function App() {
       <Route path="/settings" element={<Settings />} />
       <Route path="/library" element={<Library />} />
       <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>    
+    </Routes>   
+    
+    <LoginModal open={loginOpen} onClose={closeLogin} onOpenSignUp={openSignUp} />
+    <SignUpModal open={signUpOpen} onClose={closeSignUp} onGoToLogin={openLogin} />
+    </>
   );
 }
 
